c3f234aa37b3dfd83e553399b231840c21bd3e09
Mikhail <mail.baranov@gmail.com>
2022-03-16 18:27:34 +0300

    Add solution task 2

--- Level1.java
+++ Level1.java
@@ -1,6 +1,19 @@
-package Level1;
+package com.company;
 
 public class Level1 {
+
+    public static int odometer(int [] oksana) {
+
+        int s = 0;
+        int h = 0;
+
+        for (int i=0; i<oksana.length; i=i+2) {
+             s += (oksana[i+1]-h) * oksana[i];
+             h = oksana[i+1];
+        }
+
+        return s;
+    }
 
     public static int factorial(int N) {
         int factorial = 1;

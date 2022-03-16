243901dc17c398d8ee2d08b51e48a029f32d71b7
Mikhail <mail.baranov@gmail.com>
2022-03-16 15:32:39 +0300

    Add new solution task 1

---
+++ Level1.java
@@ -0,0 +1,23 @@
+package Level1;
+
+public class Level1 {
+
+    public static int factorial(int N) {
+        int factorial = 1;
+        for(int i=2; i<=N; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    public static int squirrel(int N) {
+
+        int emerald = factorial(N);
+
+        while( (float)emerald/10 > 1) {
+            emerald /= 10;
+        }
+
+        return emerald;
+    }
+}

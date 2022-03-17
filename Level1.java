2913214afcdfcd614d08156eb664a05594bb9be4
Mikhail <mail.baranov@gmail.com>
2022-03-17 23:01:19 +0300

    Add solution task 3

--- Level1.java
+++ Level1.java
@@ -1,6 +1,65 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Level1 {
+
+    public static boolean isAreaFill(int [][] area) {
+        for(int i=0; i<area.length; i++) {
+            for(int j=0; j<area[i].length; j++) {
+                if (area[i][j] == 0) return false;
+            }
+        }
+        return true;
+    }
+
+    public static int ConquestCampaign(int N, int M, int L, int [] battalion) {
+
+        int max = N * M;
+        int days = 0;
+
+        int [][] arealFill = new int[N][M];
+        int [][] arealFill1 = new int[N][M];
+        for(int i=0; i<N; i++)  {
+            for(int j=0; j<M; j++) {
+                arealFill[i][j] = 0;
+                arealFill1[i][j] = 0;
+            }
+        }
+
+        for(int i=0; i<battalion.length; i+=2) {
+            int x = battalion[i];
+            int y = battalion[i+1];
+            arealFill[x-1][y-1] = 1;
+            arealFill1[x-1][y-1] = 1;
+        }
+
+        boolean isFill = false;
+        while( !isFill ) {
+            isFill = true;
+            for( int i=0; i<N; i++) {
+                for (int j=0; j<M; j++) {
+
+                    int k = arealFill[i][j];
+                    if(k == 0) isFill = false;
+                    else {
+                        if (i + 1 < N) arealFill1[i + 1][j] = 1;
+                        if (i - 1 >= 0) arealFill1[i - 1][j] = 1;
+                        if (j + 1 < M) arealFill1[i][j + 1] = 1;
+                        if (j - 1 >= 0) arealFill1[i][j - 1] = 1;
+                    }
+                }
+            }
+            for (int i=0; i<N; i++) {
+                for(int j=0; j<M; j++) {
+                    arealFill[i][j] = arealFill1[i][j];
+                }
+            }
+            days++;
+        }
+
+        return days;
+    }
 
     public static int odometer(int [] oksana) {
 

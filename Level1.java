bac6ba852ce71d09ae044ad6c41a7533faf1e40b
Mikhail <mail.baranov@gmail.com>
2022-03-22 17:06:27 +0300

    Add solution task 7

--- Level1.java
+++ Level1.java
@@ -6,56 +6,68 @@ import java.util.HashMap;
 
 public class Level1 {
 
-    public static double determineDistance(int a, int b) {
-        if( (a == 1 && b == 5)
-            || (a == 1 && b == 8)
-            || (a == 2 && b == 4)
-            || (a == 2 && b == 6)
-            || (a == 2 && b == 7)
-            || (a == 2 && b == 9)
-            || (a == 3 && b == 5)
-            || (a == 3 && b == 8)
-            || (a == 4 && b == 2)
-            || (a == 5 && b == 1)
-            || (a == 5 && b == 3)
-            || (a == 6 && b == 2)
-            || (a == 7 && b == 2)
-            || (a == 8 && b == 1)
-            || (a == 8 && b == 3)
-            || (a == 9 && b == 2) ) {
+    public static int findSubStr(String str, String subStr) {
 
-            return Math.sqrt(2);
-        }
+        int lenStr = str.length();
+        int lenSubStr = subStr.length();
 
-        return 1;
-    }
-
-    public static String PatternUnlock(int N, int [] hits) {
-
-        double sum = 0;
-        for(int i=0; i<N-1; i++) {
-            sum += determineDistance(hits[i], hits[i+1]);
-        }
-
-        int sum5 = (int) (sum * 100000);
-
-        if(sum*100000 - sum5 >= 0.5)
-            sum5 += 1;
-
-        String tmp = "";
-        while( sum5 > 0 ) {
-
-            int rightNumber = sum5%10;
-            sum5 = (int)sum5/10;
-
-            if (rightNumber != 0) {
-                tmp += rightNumber;
+        for(int i=0; i<=lenStr-lenSubStr; i++) {
+            for(int j=0; j<=lenSubStr; j++) {
+                //if(j == lenSubStr)
+                if (j == lenSubStr) {
+                    if (j == lenStr || str.charAt(j) == ' ')
+                        return 1;
+                    break;
+                }
+                if(str.charAt(i+j) != subStr.charAt(j))
+                    break;
             }
         }
+        return 0;
 
-        String res = "";
-        for(int i=tmp.length()-1; i>=0; i--) {
-            res += tmp.charAt(i);
+    }
+
+    public static String[] breakIntoLines(String s, int len) {
+
+        int lenSource = s.length();
+        ArrayList<String> arr = new ArrayList<>();
+
+        for(int i=0; i<lenSource; i++) {
+
+            int start = i;
+            int end = i;
+
+            if(s.charAt(i) == ' ')
+                continue;
+
+            for(int j=start; j<=lenSource; j++) {
+
+                if((j-i == len) || (j==lenSource))
+                {
+                    if(start == end)
+                        end = j;
+
+                    i = end;
+                    break;
+                }
+
+                if(s.charAt(j) == ' ')
+                    end = j;
+            }
+            arr.add(s.substring(start, end));
+        }
+
+        return arr.toArray(new String[0]);
+    }
+
+    public static int [] WordSearch(int len, String s, String subS) {
+
+        String [] arrString = breakIntoLines(s, len);
+
+        int count = arrString.length;
+        int [] res = new int[count];
+        for(int i=0; i<count; i++) {
+            res[i] = findSubStr(arrString[i], subS);
         }
 
         return res;

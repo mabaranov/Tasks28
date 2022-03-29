97b97437eb5d08c3c057d191ab86a8ddef6fd55c
Mikhail <mail.baranov@gmail.com>
2022-03-29 12:16:56 +0300

    Add solution task 11

--- Level1.java
+++ Level1.java
@@ -6,115 +6,56 @@ import java.util.HashMap;
 
 public class Level1 {
 
-    public static int PrintingCosts(String Line) {
+    public static String BigMinus(String s1, String s2) {
 
-        HashMap<Character, Integer> symbolToner = new HashMap<>();
-        symbolToner.put(' ', 0);
-        symbolToner.put('&', 24);
-        symbolToner.put(',', 7);
-        symbolToner.put('2', 22);
-        symbolToner.put('8', 23);
-        symbolToner.put('>', 10);
-        symbolToner.put('D', 26);
-        symbolToner.put('J', 18);
-        symbolToner.put('P', 23);
-        symbolToner.put('V', 19);
-        symbolToner.put('\\', 10);
-        symbolToner.put('b', 25);
-        symbolToner.put('h', 21);
-        symbolToner.put('n', 18);
-        symbolToner.put('t', 17);
-        symbolToner.put('z', 19);
-        symbolToner.put('!', 9);
-        symbolToner.put('\'', 3);
-        symbolToner.put('-', 7);
-        symbolToner.put('3', 23);
-        symbolToner.put('9', 26);
-        symbolToner.put('?', 15);
-        symbolToner.put('E', 26);
-        symbolToner.put('K', 21);
-        symbolToner.put('Q', 31);
-        symbolToner.put('W', 26);
-        symbolToner.put(']', 18);
-        symbolToner.put('c', 17);
-        symbolToner.put('i', 15);
-        symbolToner.put('o', 20);
-        symbolToner.put('u', 17);
-        symbolToner.put('{', 18);
-        symbolToner.put('"', 6);
-        symbolToner.put('(', 12);
-        symbolToner.put('.', 4);
-        symbolToner.put('4', 21);
-        symbolToner.put(':', 8);
-        symbolToner.put('@', 32);
-        symbolToner.put('F', 20);
-        symbolToner.put('L', 16);
-        symbolToner.put('R', 28);
-        symbolToner.put('X', 18);
-        symbolToner.put('^', 7);
-        symbolToner.put('d', 25);
-        symbolToner.put('j', 20);
-        symbolToner.put('p', 25);
-        symbolToner.put('v', 13);
-        symbolToner.put('|', 12);
-        symbolToner.put('#', 24);
-        symbolToner.put(')', 12);
-        symbolToner.put('/', 10);
-        symbolToner.put('5', 27);
-        symbolToner.put(';', 11);
-        symbolToner.put('A', 24);
-        symbolToner.put('G', 25);
-        symbolToner.put('M', 28);
-        symbolToner.put('S', 25);
-        symbolToner.put('Y', 14);
-        symbolToner.put('_', 8);
-        symbolToner.put('e', 23);
-        symbolToner.put('k', 21);
-        symbolToner.put('q', 25);
-        symbolToner.put('w', 19);
-        symbolToner.put('}', 18);
-        symbolToner.put('$', 29);
-        symbolToner.put('*', 17);
-        symbolToner.put('0', 22);
-        symbolToner.put('6', 26);
-        symbolToner.put('<', 10);
-        symbolToner.put('B', 29);
-        symbolToner.put('H', 25);
-        symbolToner.put('N', 25);
-        symbolToner.put('T', 16);
-        symbolToner.put('Z', 22);
-        symbolToner.put('`', 3);
-        symbolToner.put('f', 18);
-        symbolToner.put('l', 16);
-        symbolToner.put('r', 13);
-        symbolToner.put('x', 13);
-        symbolToner.put('~', 9);
-        symbolToner.put('%', 22);
-        symbolToner.put('+', 13);
-        symbolToner.put('1', 19);
-        symbolToner.put('7', 16);
-        symbolToner.put('=', 14);
-        symbolToner.put('C', 20);
-        symbolToner.put('I', 18);
-        symbolToner.put('O', 26);
-        symbolToner.put('U', 23);
-        symbolToner.put('[', 18);
-        symbolToner.put('a', 23);
-        symbolToner.put('g', 30);
-        symbolToner.put('m', 22);
-        symbolToner.put('s', 21);
-        symbolToner.put('y', 24);
+        int count = s1.length();
+        if(count < s2.length())
+            count = s2.length();
 
-        int sumToner = 0;
-        for(int i=0; i<Line.length(); i++) {
+        int [] a = new int[count];
+        int [] b = new int[count];
 
-            char ch = Line.charAt(i);
-            Integer val = symbolToner.get(ch);
-            if(val == null) val = 23;
-            sumToner += val;
+        for(int i=0; i<count; i++) {
+
+            int n1 = 0;
+            if(i < s1.length()) n1 = Character.digit(s1.charAt(s1.length()-i-1), 10);
+            int n2 = 0;
+            if(i < s2.length()) n2 = Character.digit(s2.charAt(s2.length()-i-1), 10);
+
+            a[count-i-1] = n1;
+            b[count-i-1] = n2;
         }
 
-        return sumToner;
-    }
+        for(int i=0; i<a.length; i++) {
+            if(a[i]==b[i]) continue;
+            if(a[i]>b[i]) {
+                break;
+            } else {
+                int [] t = new int[count];
+                t = b;
+                b = a;
+                a = t;
+                break;
+            }
+        }
 
+        int [] tmp = new int[count];
+        for (int i=a.length-1; i>=0; i--) {
+            tmp[i] = a[i]-b[i];
+            if(tmp[i] < 0) {
+                a[i-1] -= 1;
+                tmp[i] += 10;
+            }
+        }
+        
+        String res = "";
+        boolean isFirstZero = true;
+        for(int i=0; i<tmp.length; i++) {
+            if(isFirstZero && tmp[i] == 0 && i!=tmp.length-1) continue;
+            isFirstZero = false;
+            res += tmp[i];
+        }
+
+        return res;
+    }
  }

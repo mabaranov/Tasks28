a7402976dbd8368cf42e5076c06ea9eca4060d48
Mikhail <mail.baranov@gmail.com>
2022-03-30 09:05:11 +0300

    Add solution task 12

--- Level1.java
+++ Level1.java
@@ -6,56 +6,34 @@ import java.util.HashMap;
 
 public class Level1 {
 
-    public static String BigMinus(String s1, String s2) {
-
-        int count = s1.length();
-        if(count < s2.length())
-            count = s2.length();
-
-        int [] a = new int[count];
-        int [] b = new int[count];
-
-        for(int i=0; i<count; i++) {
-
-            int n1 = 0;
-            if(i < s1.length()) n1 = Character.digit(s1.charAt(s1.length()-i-1), 10);
-            int n2 = 0;
-            if(i < s2.length()) n2 = Character.digit(s2.charAt(s2.length()-i-1), 10);
-
-            a[count-i-1] = n1;
-            b[count-i-1] = n2;
-        }
-
-        for(int i=0; i<a.length; i++) {
-            if(a[i]==b[i]) continue;
-            if(a[i]>b[i]) {
-                break;
-            } else {
-                int [] t = new int[count];
-                t = b;
-                b = a;
-                a = t;
-                break;
-            }
-        }
-
-        int [] tmp = new int[count];
-        for (int i=a.length-1; i>=0; i--) {
-            tmp[i] = a[i]-b[i];
-            if(tmp[i] < 0) {
-                a[i-1] -= 1;
-                tmp[i] += 10;
-            }
-        }
+    public static String MassVote(int N, int [] Votes) {
         
-        String res = "";
-        boolean isFirstZero = true;
-        for(int i=0; i<tmp.length; i++) {
-            if(isFirstZero && tmp[i] == 0 && i!=tmp.length-1) continue;
-            isFirstZero = false;
-            res += tmp[i];
+        int sum = 0;
+        int max = Votes[0];
+        for(int i=0; i<Votes.length; i++) {
+            if(max < Votes[i]) max = Votes[i];
+            sum += Votes[i];
         }
 
-        return res;
+        int countWinner = 0;
+        for(int i=0; i<Votes.length; i++) {
+            if(max == Votes[i]) countWinner++;
+        }
+
+        double per = max*100.0/sum;
+
+        int tmp = (int)(per*1000);
+        if(per*1000 - tmp >= 0.5)
+            per = (tmp+1)/1000;
+        else
+            per = tmp*1.0/1000;
+
+        if(countWinner == 1 && per > 50)
+            return "majority winner 1";
+
+        if(countWinner == 1)
+            return "minority winner 2";
+
+        return "no winner";
     }
  }

90db8cd760ff2c6e927e9b8532ea382c6b52965b
Mikhail <mail.baranov@gmail.com>
2022-04-04 17:13:14 +0300

    Add solution task 15

--- Level1.java
+++ Level1.java
@@ -6,68 +6,74 @@ import java.util.HashMap;
 
 public class Level1 {
 
-    public static int Unmanned(int L, int N, int [][] track) {
+    private static int [][] stringToArray(int h, int w, String s) {
 
-        int time = 0;
+        int [][] res = new int [h][w];
+        String tmp = s;
+        for (int i=0; i<h; i++) {
 
-        for(int i=0; i<=N; i++) {
-
-            if(i == N) {
-                time += L-track[i-1][0];
-                break;
+            int j = 0;
+            while(j<tmp.length() && tmp.charAt(j) != ' ') {
+                res[i][j] = Character.getNumericValue(tmp.charAt(j));
+                j += 1;
             }
-
-            int [] pCur = track[i];
-            int [] pPrev = {0,0,0};
-            if(i>0)
-                pPrev = track[i-1];
-
-            if(pCur[0] > L) {
-                time += L - pPrev[0];
-                break;
-            }
-
-            time += pCur[0]-pPrev[0];
-
-            boolean isRed = true;
-            int t = time;
-            while(t > 0) {
-                if(isRed) {
-                    t -= pCur[1];
-                    isRed = false;
-                } else {
-                    t -= pCur[2];
-                    isRed = true;
-                }
-            }
-            if(isRed == false) time = time - t;
+            if(j+1 < tmp.length())
+                tmp = tmp.substring(j+1);
         }
 
-        return time;
+        return res;
     }
 
+    private static int [][] copySubArray(int start_i, int start_j, int count_i, int count_j, int size_i, int size_j, int [][] source) {
 
-//    public static int [] UFO(int N, int [] data, boolean octal) {
-//
-//        int base = octal ? 8 : 16;
-//
-//        int [] res = new int[N];
-//
-//        for(int i=0; i<N; i++) {
-//
-//            int nBase = data[i];
-//            int n10 = 0;
-//            int pow = 0;
-//            while( nBase >= 1 ) {
-//                int n = nBase % 10;
-//                n10 += n * Math.pow(base, pow);
-//                nBase /= 10;
-//                pow += 1;
-//            }
-//
-//            res[i] = n10;
-//        }
-//
-//        return res;
-//    }
- }
+        int [][] res = new int [count_i][count_j];
+
+        for(int i=0; i<count_i; i++) {
+            for(int j=0; j<count_j; j++) {
+                if (start_i+i < size_i && start_j+j < size_j)
+                    res[i][j] = source[start_i+i][start_j+j];
+            }
+        }
+
+        return res;
+    }
+
+    private static boolean compareArray(int size_i, int size_j, int [][] source, int [][] target) {
+
+        for (int i=0; i<size_i; i++) {
+            for (int j=0; j<size_j; j++) {
+                if( source[i][j] != target[i][j]) return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean TankRush(int H1, int W1, String S1, int H2, int W2, String S2) {
+
+        int [][] map = stringToArray(H1, W1, S1);
+        int [][] tank = stringToArray(H2, W2, S2);
+
+        int delta_i = 0;
+        int delta_j = 0;
+        boolean isFind = false;
+
+        for (int i=0; i<H1; i++) {
+            for (int j=0; j<W1; j++) {
+
+                int source = map[i][j];
+                int target = tank[delta_i][delta_j];
+
+                if (source == target) {
+                    int [][] subMap = copySubArray(i, j, H2, W2, H1, W1, map);
+                    isFind = compareArray(H2, W2, subMap, tank);
+                    if (isFind) break;
+                };
+
+
+            }
+            if (isFind) break;
+        }
+
+        return isFind;
+    }
+  }

45b400b8ac73c7bcce3b3a9f551fa7455cbfef57
Mikhail <mail.baranov@gmail.com>
2022-04-05 21:41:53 +0300

    Add solution task 16

--- Level1.java
+++ Level1.java
@@ -6,74 +6,30 @@ import java.util.HashMap;
 
 public class Level1 {
 
-    private static int [][] stringToArray(int h, int w, String s) {
+    private static void bubble_sort(int N, int [] price) {
 
-        int [][] res = new int [h][w];
-        String tmp = s;
-        for (int i=0; i<h; i++) {
-
-            int j = 0;
-            while(j<tmp.length() && tmp.charAt(j) != ' ') {
-                res[i][j] = Character.getNumericValue(tmp.charAt(j));
-                j += 1;
+        for(int i=N-1; i>0; i--) {
+            for(int j=0; j<i; j++) {
+                if(price[j] < price[j+1]) {
+                    int tmp = price[j];
+                    price[j] = price[j+1];
+                    price[j+1] = tmp;
+                }
             }
-            if(j+1 < tmp.length())
-                tmp = tmp.substring(j+1);
         }
 
-        return res;
     }
 
-    private static int [][] copySubArray(int start_i, int start_j, int count_i, int count_j, int size_i, int size_j, int [][] source) {
+    public static int MaximumDiscount(int N, int [] price) {
 
-        int [][] res = new int [count_i][count_j];
+        bubble_sort(N, price);
 
-        for(int i=0; i<count_i; i++) {
-            for(int j=0; j<count_j; j++) {
-                if (start_i+i < size_i && start_j+j < size_j)
-                    res[i][j] = source[start_i+i][start_j+j];
-            }
+        int discount = 0;
+        for (int i=1; i<=N; i++) {
+            if (i % 3 == 0)
+                discount += price[i-1];
         }
 
-        return res;
-    }
-
-    private static boolean compareArray(int size_i, int size_j, int [][] source, int [][] target) {
-
-        for (int i=0; i<size_i; i++) {
-            for (int j=0; j<size_j; j++) {
-                if( source[i][j] != target[i][j]) return false;
-            }
-        }
-        return true;
-    }
-
-    public static boolean TankRush(int H1, int W1, String S1, int H2, int W2, String S2) {
-
-        int [][] map = stringToArray(H1, W1, S1);
-        int [][] tank = stringToArray(H2, W2, S2);
-
-        int delta_i = 0;
-        int delta_j = 0;
-        boolean isFind = false;
-
-        for (int i=0; i<H1; i++) {
-            for (int j=0; j<W1; j++) {
-
-                int source = map[i][j];
-                int target = tank[delta_i][delta_j];
-
-                if (source == target) {
-                    int [][] subMap = copySubArray(i, j, H2, W2, H1, W1, map);
-                    isFind = compareArray(H2, W2, subMap, tank);
-                    if (isFind) break;
-                };
-
-
-            }
-            if (isFind) break;
-        }
-
-        return isFind;
+        return discount;
     }
   }
